6756b95845107051e2b6d93519317b575574ab28
Walter Bender <walter@sugarlabs.org>
2010-08-26 14:01:26 -0400

    using combo boxes instead of buttons

--- SlideruleActivity.py
+++ SlideruleActivity.py
@@ -31,6 +31,8 @@ if _have_toolbox:
     from sugar.activity.widgets import StopButton
     from sugar.graphics.toolbarbox import ToolbarButton
 
+from sugar.graphics.combobox import ComboBox
+from sugar.graphics.toolcombobox import ToolComboBox
 from sugar.graphics.toolbutton import ToolButton
 from sugar.graphics.menuitem import MenuItem
 from sugar.graphics.icon import Icon
@@ -45,6 +47,47 @@ _logger = logging.getLogger('sliderule-activity')
 
 from window import SlideRule
 from constants import SWIDTH
+
+_FA = _('square/square root')
+_FC = _('multiply/divide')
+_FCI = _('inverse')
+_FK = _('cube/cube root')
+_FS = _('sin, asin')
+_FT = _('tan, atan')
+_FL = _('add/subtract')
+_UD = _('user defined')
+_FUNCTIONS = [_FL, _FC, _FCI, _FA, _FK, _FS, _FT, _UD]
+
+_A = _('log²')
+_C = _('log')
+_CI = _('1/log')
+_K = _('log³')
+_S = _('sin')
+_T = _('tan')
+_L = _('linear')
+_TOP_SCALES = [_L, _C, _CI, _A, _K, _S, _T]
+
+_D = _('log')
+_CI2 = _('1/log')
+_L2 = _('linear')
+_BOT_SCALES = [_L2, _D, _CI2]
+
+
+def _combo_factory(combo_array, default, tooltip, callback, toolbar):
+    """Factory for making a toolbar combo box"""
+    my_combo = ComboBox()
+    if hasattr(my_combo, 'set_tooltip_text'):
+        my_combo.set_tooltip_text(tooltip)
+
+    my_combo.connect('changed', callback)
+
+    for i, s in enumerate(combo_array):
+        my_combo.append_item(i, s, None)
+        if s == default:
+            my_combo.set_active(i)
+
+    toolbar.insert(ToolComboBox(my_combo), -1)
+    return my_combo
 
 
 def _button_factory(icon_name, tooltip, callback, toolbar, cb_arg=None,
@@ -65,6 +108,18 @@ def _button_factory(icon_name, tooltip, callback, toolbar, cb_arg=None,
         toolbar.props.page.insert(my_button, -1)
     my_button.show()
     return my_button
+
+
+def _label_factory(label, toolbar):
+    """ Factory for adding a label to a toolbar """
+    my_label = gtk.Label(label)
+    my_label.set_line_wrap(True)
+    my_label.show()
+    _toolitem = gtk.ToolItem()
+    _toolitem.add(my_label)
+    toolbar.insert(_toolitem, -1)
+    _toolitem.show()
+    return my_label
 
 
 def _separator_factory(toolbar, visible=True, expand=False):
@@ -126,11 +181,18 @@ class SlideruleActivity(activity.Activity):
             self.sr.R_tab_top.spr.move_relative((int(self.metadata['R']), 0))
             self.sr.R_tab_bot.spr.move_relative((int(self.metadata['R']), 0))
         if 'slider' in self.metadata:
+            _logger.debug("restoring %s" % (self.metadata['slider']))
             self.sr.slider_on_top = self.metadata['slider']
             if self.sr.slider_on_top == 'A':
                 self._show_a()
             elif self.sr.slider_on_top == 'L':
                 self._show_l()
+            elif self.sr.slider_on_top == 'K':
+                self._show_k()
+            elif self.sr.slider_on_top == 'S':
+                self._show_s()
+            elif self.sr.slider_on_top == 'T':
+                self._show_t()
             elif self.sr.slider_on_top == 'CI':
                 self._show_ci()
             else:
@@ -138,13 +200,23 @@ class SlideruleActivity(activity.Activity):
             self.sr.update_results_label()
             self.sr.update_slider_labels()
 
+    def write_file(self, file_path):
+        """ Write the slider positions to the Journal """
+        self.metadata['slider'] = self.sr.slider_on_top
+        x, y = self.sr.C.spr.get_xy()
+        self.metadata['C'] = str(x)
+        x, y = self.sr.CI.spr.get_xy()
+        self.metadata['CI'] = str(x)
+        x, y = self.sr.D.spr.get_xy()
+        self.metadata['D'] = str(x)
+        x, y = self.sr.R.spr.get_xy()
+        self.metadata['R'] = str(x)
+        x, y = self.sr.L.spr.get_xy()
+        self.metadata['L'] = str(x)
+        x, y = self.sr.L2.spr.get_xy()
+        self.metadata['L2'] = str(x)
+
     def _hide_all(self):
-        self.a_slider.set_icon('Aoff')
-        self.k_slider.set_icon('Koff')
-        self.s_slider.set_icon('Soff')
-        self.c_slider.set_icon('Coff')
-        self.ci_slider.set_icon('CIoff')
-        self.l_slider.set_icon('Loff')
         self.sr.A.spr.hide()
         self.sr.K.spr.hide()
         self.sr.S.spr.hide()
@@ -161,13 +233,13 @@ class SlideruleActivity(activity.Activity):
         self.sr.L2_tab_left.spr.hide()
         self.sr.L2_tab_right.spr.hide()
 
-    def _c_slider_cb(self, button):
-        self._show_c()
-        return True
-
     def _show_c(self):
         self._hide_all()
-        self.c_slider.set_icon('Con')
+        self.t_button.set_icon('Con')
+        self.b_button.set_icon('Don')
+        self._top_combo.set_active(_TOP_SCALES.index(_C))
+        self._bot_combo.set_active(_BOT_SCALES.index(_D))
+        self._function_combo.set_active(_FUNCTIONS.index(_FC))
         self.sr.C.draw_slider(1000)
         self.sr.C_tab_left.draw_slider(1000)
         self.sr.C_tab_right.draw_slider(1000)
@@ -176,13 +248,13 @@ class SlideruleActivity(activity.Activity):
         self.sr.update_slider_labels()
         self.sr.update_results_label()
 
-    def _ci_slider_cb(self, button):
-        self._show_ci()
-        return True
-
     def _show_ci(self):
         self._hide_all()
-        self.ci_slider.set_icon('CIon')
+        self.t_button.set_icon('CIon')
+        self.b_button.set_icon('Don')
+        self._top_combo.set_active(_TOP_SCALES.index(_CI))
+        self._bot_combo.set_active(_BOT_SCALES.index(_D))
+        self._function_combo.set_active(_FUNCTIONS.index(_FCI))
         self.sr.CI.draw_slider(1000)
         self.sr.CI_tab_left.draw_slider(1000)
         self.sr.CI_tab_right.draw_slider(1000)
@@ -191,65 +263,65 @@ class SlideruleActivity(activity.Activity):
         self.sr.update_slider_labels()
         self.sr.update_results_label()
 
-    def _a_slider_cb(self, button):
-        self._show_a()
-        return True
-
     def _show_a(self):
         self._hide_all()
-        self.a_slider.set_icon('Aon')
+        self.t_button.set_icon('Aon')
+        self.b_button.set_icon('Don')
+        self._top_combo.set_active(_TOP_SCALES.index(_A))
+        self._bot_combo.set_active(_BOT_SCALES.index(_D))
+        self._function_combo.set_active(_FUNCTIONS.index(_FA))
         self.sr.A.draw_slider(1000)
         self.sr.D.draw_slider(1000)
         self.sr.slider_on_top = 'A'
         self.sr.update_slider_labels()
         self.sr.update_results_label()
 
-    def _k_slider_cb(self, button):
-        self._show_k()
-        return True
-
     def _show_k(self):
         self._hide_all()
-        self.k_slider.set_icon('Kon')
+        self.t_button.set_icon('Kon')
+        self.b_button.set_icon('Don')
+        self._top_combo.set_active(_TOP_SCALES.index(_K))
+        self._bot_combo.set_active(_BOT_SCALES.index(_D))
+        self._function_combo.set_active(_FUNCTIONS.index(_FK))
         self.sr.K.draw_slider(1000)
         self.sr.D.draw_slider(1000)
         self.sr.slider_on_top = 'K'
         self.sr.update_slider_labels()
         self.sr.update_results_label()
 
-    def _s_slider_cb(self, button):
-        self._show_s()
-        return True
-
     def _show_s(self):
         self._hide_all()
-        self.s_slider.set_icon('Son')
+        self.t_button.set_icon('Son')
+        self.b_button.set_icon('Don')
+        self._top_combo.set_active(_TOP_SCALES.index(_S))
+        self._bot_combo.set_active(_BOT_SCALES.index(_D))
+        self._function_combo.set_active(_FUNCTIONS.index(_FS))
         self.sr.S.draw_slider(1000)
         self.sr.D.draw_slider(1000)
         self.sr.slider_on_top = 'S'
         self.sr.update_slider_labels()
         self.sr.update_results_label()
 
-    def _t_slider_cb(self, button):
-        self._show_t()
-        return True
-
     def _show_t(self):
         self._hide_all()
-        self.t_slider.set_icon('Ton')
+        self.t_button.set_icon('Ton')
+        self.b_button.set_icon('Don')
+        self._top_combo.set_active(_TOP_SCALES.index(_T))
+        self._bot_combo.set_active(_BOT_SCALES.index(_D))
+        self._function_combo.set_active(_FUNCTIONS.index(_FT))
         self.sr.T.draw_slider(1000)
         self.sr.D.draw_slider(1000)
         self.sr.slider_on_top = 'T'
         self.sr.update_slider_labels()
         self.sr.update_results_label()
 
-    def _l_slider_cb(self, button):
-        self._show_l()
-        return True
-
     def _show_l(self):
         self._hide_all()
-        self.l_slider.set_icon('Lon')
+        self.t_button.set_icon('Lon')
+        self.b_button.set_icon('Lon')
+        self._top_combo.set_active(_TOP_SCALES.index(_L))
+        self._bot_combo.set_active(_BOT_SCALES.index(_L2))
+        self._function_combo.set_active(_FUNCTIONS.index(_FL))
         self.sr.L.draw_slider(1000)
         self.sr.L2.draw_slider(1000)
         self.sr.L2_tab_left.draw_slider(1000)
@@ -258,23 +330,34 @@ class SlideruleActivity(activity.Activity):
         self.sr.update_slider_labels()
         self.sr.update_results_label()
 
-    """
-    Write the slider positions to the Journal
-    """
-    def write_file(self, file_path):
-        self.metadata['slider'] = self.sr.slider_on_top
-        x, y = self.sr.C.spr.get_xy()
-        self.metadata['C'] = str(x)
-        x, y = self.sr.CI.spr.get_xy()
-        self.metadata['CI'] = str(x)
-        x, y = self.sr.D.spr.get_xy()
-        self.metadata['D'] = str(x)
-        x, y = self.sr.R.spr.get_xy()
-        self.metadata['R'] = str(x)
-        x, y = self.sr.L.spr.get_xy()
-        self.metadata['L'] = str(x)
-        x, y = self.sr.L2.spr.get_xy()
-        self.metadata['L2'] = str(x)
+    def _top_combo_cb(self, arg=None):
+        """ Read value from top combo box """
+        top_dictionary = {_A: self._show_a, _C: self._show_c, _K: self._show_k,
+                          _S: self._show_s, _T: self._show_t, _L: self._show_l, 
+                          _CI: self._show_ci} 
+        if hasattr(self, '_top_combo'):
+            top_dictionary[_TOP_SCALES[self._top_combo.get_active()]]()
+        else:
+            _logger.debug("no top_combo yet")
+
+    def _fun_combo_cb(self, arg=None):
+        """ Read value from function combo box """
+        fun_dictionary = {_FA: self._show_a, _FC: self._show_c,
+                          _FK: self._show_k, _FS: self._show_s,
+                          _FT: self._show_t, _FL: self._show_l, 
+                          _FCI: self._show_ci} 
+        if hasattr(self, '_function_combo'):
+            fun_dictionary[_FUNCTIONS[self._function_combo.get_active()]]()
+        else:
+            _logger.debug("no function_combo yet")
+
+
+    def _bot_combo_cb(self, arg=None):
+        #TODO: reset function combo
+        return
+
+    def _dummy_cb(self, arg=None):
+        return
 
     def _setup_toolbars(self, have_toolbox):
         """ Setup the toolbars.. """
@@ -303,20 +386,17 @@ class SlideruleActivity(activity.Activity):
             toolbar = project_toolbar
 
         # Add the buttons to the toolbars
-        self.l_slider = _button_factory('Loff', _('add/subtract'),
-                                        self._l_slider_cb, toolbar)
-        self.c_slider = _button_factory('Con', _('multiply/divide'),
-                                        self._c_slider_cb, toolbar)
-        self.ci_slider = _button_factory('CIoff', _('inverse'),
-                                        self._ci_slider_cb, toolbar)
-        self.a_slider = _button_factory('Aoff', _('square/square root'),
-                                        self._a_slider_cb, toolbar)
-        self.k_slider = _button_factory('Koff', _('cube/cube root'),
-                                        self._k_slider_cb, toolbar)
-        self.s_slider = _button_factory('Soff', _('sin, asin'),
-                                        self._s_slider_cb, toolbar)
-        self.t_slider = _button_factory('Toff', _('tan, atan'),
-                                        self._t_slider_cb, toolbar)
+        self.t_button = _button_factory('Con', _('top scale'), self._dummy_cb,
+                                        toolbar)
+        self._top_combo = _combo_factory(_TOP_SCALES, _C, _('top scale'),
+                                         self._top_combo_cb, toolbar)
+        self.b_button = _button_factory('Don', _('bottom scale'),
+                                        self._dummy_cb, toolbar)
+        self._bot_combo = _combo_factory(_BOT_SCALES, _D, _('bot scale'),
+                                         self._bot_combo_cb, toolbar)
+        _separator_factory(toolbar)
+        self._function_combo = _combo_factory(_FUNCTIONS, _FC, _('function'),
+                                              self._fun_combo_cb, toolbar)
 
         if _have_toolbox:
             _separator_factory(toolbox.toolbar, False, True)
