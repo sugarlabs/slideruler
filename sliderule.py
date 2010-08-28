1b364957dc4c861c22605d0cd6e51f7dc9b144c6
Walter Bender <walter@sugarlabs.org>
2010-08-28 09:48:42 -0400

    updated to include new slides

--- sliderule.py
+++ sliderule.py
@@ -23,7 +23,7 @@ from window import SlideRule
 
 _FA = _('square/square root')
 _FC = _('multiply/divide')
-_FCI = _('inverse')
+_FCI = _('divide/multiply')
 _FK = _('cube/cube root')
 _FS = _('sin, asin')
 _FT = _('tan, atan')
@@ -70,6 +70,10 @@ class SlideruleMain:
         menu.append(menu_items)
         menu_items.connect("activate", self._t_cb)
         menu_items.show()
+        menu_items = gtk.MenuItem(_("realign slides"))
+        menu.append(menu_items)
+        menu_items.connect("activate", self._realign_cb)
+        menu_items.show()
         root_menu = gtk.MenuItem("Tools")
         root_menu.show()
         root_menu.set_submenu(menu)
@@ -96,22 +100,77 @@ class SlideruleMain:
         self.hide_all()
         self._c_cb(None)
 
+    def set_title(self, title):
+        self.win.set_title(title)
+
     def hide_all(self):
         self.sr.A.spr.hide()
         self.sr.K.spr.hide()
         self.sr.S.spr.hide()
         self.sr.T.spr.hide()
+        self.sr.A2.spr.hide()
+        self.sr.K2.spr.hide()
+        self.sr.S2.spr.hide()
+        self.sr.T2.spr.hide()
+        self.sr.D.spr.hide()
         self.sr.C.spr.hide()
         self.sr.C_tab_left.spr.hide()
         self.sr.C_tab_right.spr.hide()
         self.sr.CI.spr.hide()
-        self.sr.L.spr.hide()
+        self.sr.CI_tab_left.spr.hide()
+        self.sr.CI_tab_right.spr.hide()
+        self.sr.A_tab_left.spr.hide()
+        self.sr.A_tab_right.spr.hide()
+        self.sr.K_tab_left.spr.hide()
+        self.sr.K_tab_right.spr.hide()
+        self.sr.S_tab_left.spr.hide()
+        self.sr.S_tab_right.spr.hide()
+        self.sr.T_tab_left.spr.hide()
+        self.sr.T_tab_right.spr.hide()
         self.sr.L2.spr.hide()
-        self.sr.L2_tab_left.spr.hide()
-        self.sr.L2_tab_right.spr.hide()
+        self.sr.L.spr.hide()
+        self.sr.L_tab_left.spr.hide()
+        self.sr.L_tab_right.spr.hide()
 
-    def set_title(self, title):
-        self.win.set_title(title)
+    def _realign_cb(self, arg=None):
+        """ Realign all sliders with the D scale. """
+        dx, dy = self.sr.D.spr.get_xy()
+        cx, cy = self.sr.C.spr.get_xy()
+        ax, y = self.sr.A.spr.get_xy()
+        ix, y = self.sr.CI.spr.get_xy()
+        kx, y = self.sr.K.spr.get_xy()
+        sx, y = self.sr.S.spr.get_xy()
+        tx, y = self.sr.T.spr.get_xy()
+        lx, y = self.sr.L.spr.get_xy()
+        self.sr.C.spr.move((dx, cy))
+        self.sr.CI.spr.move((dx, cy))
+        self.sr.A.spr.move((dx, cy))
+        self.sr.K.spr.move((dx, cy))
+        self.sr.S.spr.move((dx, cy))
+        self.sr.T.spr.move((dx, cy))
+        self.sr.L.spr.move((dx, cy))
+        self.sr.L2.spr.move((dx, dy))
+        self.sr.A2.spr.move((dx, dy))
+        self.sr.K2.spr.move((dx, dy))
+        self.sr.S2.spr.move((dx, dy))
+        self.sr.T2.spr.move((dx, dy))
+        self.sr.DI.spr.move((dx, dy))
+        self.sr.C_tab_left.spr.move_relative((dx-cx, 0))
+        self.sr.C_tab_right.spr.move_relative((dx-cx, 0))
+        self.sr.CI_tab_left.spr.move_relative((dx-ix, 0))
+        self.sr.CI_tab_right.spr.move_relative((dx-ix, 0))
+        self.sr.A_tab_left.spr.move_relative((sx-ax, 0))
+        self.sr.A_tab_right.spr.move_relative((dx-ax, 0))
+        self.sr.K_tab_left.spr.move_relative((dx-kx, 0))
+        self.sr.K_tab_right.spr.move_relative((dx-kx, 0))
+        self.sr.S_tab_left.spr.move_relative((dx-sx, 0))
+        self.sr.S_tab_right.spr.move_relative((dx-sx, 0))
+        self.sr.T_tab_left.spr.move_relative((dx-tx, 0))
+        self.sr.T_tab_right.spr.move_relative((dx-tx, 0))
+        self.sr.L_tab_left.spr.move_relative((dx-lx, 0))
+        self.sr.L_tab_right.spr.move_relative((dx-lx, 0))
+        self.sr.update_slider_labels()
+        self.sr.update_results_label()
 
     def _c_cb(self, widget):
         self.hide_all()
@@ -128,9 +187,11 @@ class SlideruleMain:
     def _ci_cb(self, widget):
         self.hide_all()
         self.sr.CI.draw_slider(1000)
-        self.sr.C.draw_slider(1000)
-        self.sr.slider_on_bottom = "CI"
-        self.sr.slider_on_top = "C"
+        self.sr.CI_tab_left.draw_slider(1000)
+        self.sr.CI_tab_right.draw_slider(1000)
+        self.sr.D.draw_slider(1000)
+        self.sr.slider_on_top = "CI"
+        self.sr.slider_on_bottom = "D"
         self.sr.update_slider_labels()
         self.sr.update_results_label()
         return True
@@ -138,6 +199,8 @@ class SlideruleMain:
     def _a_cb(self, widget):
         self.hide_all()
         self.sr.A.draw_slider(1000)
+        self.sr.A_tab_left.draw_slider(1000)
+        self.sr.A_tab_right.draw_slider(1000)
         self.sr.D.draw_slider(1000)
         self.sr.slider_on_top = "A"
         self.sr.slider_on_bottom = "D"
@@ -148,6 +211,8 @@ class SlideruleMain:
     def _k_cb(self, widget):
         self.hide_all()
         self.sr.K.draw_slider(1000)
+        self.sr.K_tab_left.draw_slider(1000)
+        self.sr.K_tab_right.draw_slider(1000)
         self.sr.D.draw_slider(1000)
         self.sr.slider_on_top = "K"
         self.sr.slider_on_bottom = "D"
@@ -158,6 +223,8 @@ class SlideruleMain:
     def _s_cb(self, widget):
         self.hide_all()
         self.sr.S.draw_slider(1000)
+        self.sr.S_tab_left.draw_slider(1000)
+        self.sr.S_tab_right.draw_slider(1000)
         self.sr.D.draw_slider(1000)
         self.sr.slider_on_top = "S"
         self.sr.slider_on_bottom = "D"
@@ -168,6 +235,8 @@ class SlideruleMain:
     def _t_cb(self, widget):
         self.hide_all()
         self.sr.T.draw_slider(1000)
+        self.sr.T_tab_left.draw_slider(1000)
+        self.sr.T_tab_right.draw_slider(1000)
         self.sr.D.draw_slider(1000)
         self.sr.slider_on_top = "T"
         self.sr.slider_on_bottom = "D"
@@ -177,12 +246,12 @@ class SlideruleMain:
 
     def _l_cb(self, widget):
         self.hide_all()
-        self.sr.L.draw_slider(1000)
         self.sr.L2.draw_slider(1000)
-        self.sr.L2_tab_left.draw_slider(1000)
-        self.sr.L2_tab_right.draw_slider(1000)
-        self.sr.slider_on_top = "L2"
-        self.sr.slider_on_bottom = "L"
+        self.sr.L.draw_slider(1000)
+        self.sr.L_tab_left.draw_slider(1000)
+        self.sr.L_tab_right.draw_slider(1000)
+        self.sr.slider_on_top = "L"
+        self.sr.slider_on_bottom = "L2"
         self.sr.update_slider_labels()
         self.sr.update_results_label()
         return True
