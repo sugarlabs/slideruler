8a7021252863fd4aa1f7a2342880106289e19342
Walter Bender <walter@sugarlabs.org>
2010-08-30 09:25:58 -0400

    clean up, relabelling

--- A.py
+++ A.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#Copyright (c) 2009, Walter Bender
+#Copyright (c) 2009, 10 Walter Bender
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -11,131 +11,71 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
-from constants import *
+from constants import SWIDTH, SHEIGHT, OFFSET, SCALE, HTOP1, HTOP2, HTOP3
+from C import header, footer, mark, special_mark
+
 import math
 
-# header
-print "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>"
-print "<!-- Created with Emacs -->"
-print "<svg"
-print "   xmlns:svg=\"http://www.w3.org/2000/svg\""
-print "   xmlns=\"http://www.w3.org/2000/svg\""
-print "   version=\"1.0\""
-print "   width=\"" + str(SWIDTH) + "\""
-print "   height=\"" + str(SHEIGHT) + "\">"
-print "  <g>"
-
-print "   <path"
-print "       d=\"M 0.0,30 L 2400,30\""
-print "       style=\"fill:none;stroke:#ffffff;stroke-width:60px;stroke-linecap:square;stroke-linejoin:miter;stroke-opacity:1\" />"
-
-print "  <text style=\"font-size:12px;fill:#000000;\">"
-print "      <tspan"
-print "       x=\"5\""
-print "       y=\"32\""
-print "       style=\"font-size:12px;text-align:center;text-anchor:middle;font-family:Bitstream Vera Sans;\">A</tspan></text>"
+htop1 = HTOP1
+htop2 = HTOP2
+htop3 = HTOP3
+offset1 = 5
+offset2 = 7
+offset3 = -12
 
 
-ln = str(float(int( (math.log(math.pi)*SCALE/2 + OFFSET)*10 )/10.))
-h1 = "38"; h2 = "59"; h3 = "35"
-print "  <text style=\"font-size:12px;fill:#000000;\">"
-print "      <tspan"
-print "       x=\"" + str(ln) + "\""
-print "       y=\"" + h3 + "\""
-print "       style=\"font-size:12px;text-align:center;text-anchor:middle;font-family:Bitstream Vera Sans;\">π</tspan></text>"
-print "   <path"
-print "       d=\"M " + ln + "," + h1 + " L " + ln + "," + h2 + "\""
-print "       style=\"fill:none;stroke:#FF0000;stroke-width:1px;stroke-linecap:square;stroke-linejoin:miter;stroke-opacity:1\" />"
+def main():
 
-ln = str(float(int( (math.log(math.e)*SCALE/2 + OFFSET)*10 )/10.))
-h1 = "38"; h2 = "59"; h3 = "35"
-print "  <text style=\"font-size:12px;fill:#000000;\">"
-print "      <tspan"
-print "       x=\"" + str(ln) + "\""
-print "       y=\"" + h3 + "\""
-print "       style=\"font-size:12px;text-align:center;text-anchor:middle;font-family:Bitstream Vera Sans;\">e</tspan></text>"
-print "   <path"
-print "       d=\"M " + ln + "," + h1 + " L " + ln + "," + h2 + "\""
-print "       style=\"fill:none;stroke:#FF0000;stroke-width:1px;stroke-linecap:square;stroke-linejoin:miter;stroke-opacity:1\" />"
+    header('A')
 
-for i in range(100,200):
-    ln = str(float(int( (math.log(i/100.)*SCALE/2 + OFFSET)*10 )/10.))
-    if int((i/10)*10) == i:
-        h1 = "38"; h2 = "59"; h3 = "35"
-        if int((i/100)*100) == i:
-            print "  <text style=\"font-size:12px;fill:#000000;\">"
-            print "      <tspan"
-            print "       x=\"" + str(ln) + "\""
-            print "       y=\"" + h3 + "\""
-            print "       style=\"font-size:12px;text-align:center;text-anchor:middle;font-family:Bitstream Vera Sans;\">" + str(int(i*10/SCALE)) + "</tspan></text>"
-    elif int((i/5)*5) == i:
-        h1 = "40"; h2 = "59";
-    else:
-        h1 = "42"; h2 = "59";
-    print "   <path"
-    print "       d=\"M " + ln + "," + h1 + " L " + ln + "," + h2 + "\""
-    print "       style=\"fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:square;stroke-linejoin:miter;stroke-opacity:1\" />"
-for i in range(200,400,2):
-    ln = str(float(int( (math.log(i/100.)*SCALE/2 + OFFSET)*10 )/10.))
-    if int((i/10)*10) == i:
-        h1 = "38"; h2 = "59"; h3 = "35"
-        if int((i/100)*100) == i:
-            print "  <text style=\"font-size:12px;fill:#000000;\">"
-            print "      <tspan"
-            print "       x=\"" + str(ln) + "\""
-            print "       y=\"" + h3 + "\""
-            print "       style=\"font-size:12px;text-align:center;text-anchor:middle;font-family:Bitstream Vera Sans;\">" + str(int(i*10/SCALE)) + "</tspan></text>"
-    else:
-        h1 = "40"; h2 = "59";
-    print "   <path"
-    print "       d=\"M " + ln + "," + h1 + " L " + ln + "," + h2 + "\""
-    print "       style=\"fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:square;stroke-linejoin:miter;stroke-opacity:1\" />"
-for i in range(400,1000,5):
-    ln = str(float(int( (math.log(i/100.)*SCALE/2 + OFFSET)*10 )/10.))
-    if int((i/10)*10) == i:
-        h1 = "38"; h2 = "59"; h3 = "35"
-        if int((i/100)*100) == i:
-            print "  <text style=\"font-size:12px;fill:#000000;\">"
-            print "      <tspan"
-            print "       x=\"" + str(ln) + "\""
-            print "       y=\"" + h3 + "\""
-            print "       style=\"font-size:12px;text-align:center;text-anchor:middle;font-family:Bitstream Vera Sans;\">" + str(int(i*10/SCALE)) + "</tspan></text>"
-    else:
-        h1 = "40"; h2 = "59";
-    print "   <path"
-    print "       d=\"M " + ln + "," + h1 + " L " + ln + "," + h2 + "\""
-    print "       style=\"fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:square;stroke-linejoin:miter;stroke-opacity:1\" />"
-for i in range(1000,2000,10):
-    ln = str(float(int( (math.log(i/100.)*SCALE/2 + OFFSET)*10 )/10.))
-    if int((i/10)*10) == i:
-        h1 = "38"; h2 = "59"; h3 = "35"
-        if int((i/100)*100) == i:
-            print "  <text style=\"font-size:12px;fill:#000000;\">"
-            print "      <tspan"
-            print "       x=\"" + str(ln) + "\""
-            print "       y=\"" + h3 + "\""
-            print "       style=\"font-size:12px;text-align:center;text-anchor:middle;font-family:Bitstream Vera Sans;\">" + str(int(i*10/SCALE)) + "</tspan></text>"
-    else:
-        h1 = "40"; h2 = "59";
-    print "   <path"
-    print "       d=\"M " + ln + "," + h1 + " L " + ln + "," + h2 + "\""
-    print "       style=\"fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:square;stroke-linejoin:miter;stroke-opacity:1\" />"
-for i in range(2000,10050,50):
-    ln = str(float(int( (math.log(i/100.)*SCALE/2 + OFFSET)*10 )/10.))
-    if int((i/500)*500) == i:
-        h1 = "38"; h2 = "59"; h3 = "35"
-        if int((i/100)*100) == i:
-            print "  <text style=\"font-size:12px;fill:#000000;\">"
-            print "      <tspan"
-            print "       x=\"" + str(ln) + "\""
-            print "       y=\"" + h3 + "\""
-            print "       style=\"font-size:12px;text-align:center;text-anchor:middle;font-family:Bitstream Vera Sans;\">" + str(int(i*10/SCALE)) + "</tspan></text>"
-    else:
-        h1 = "40"; h2 = "59";
-    print "   <path"
-    print "       d=\"M " + ln + "," + h1 + " L " + ln + "," + h2 + "\""
-    print "       style=\"fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:square;stroke-linejoin:miter;stroke-opacity:1\" />"
+    for i in range(100, 200):
+        if int((i / 10) * 10) == i:
+            mark(math.log(i / 100.) / 2, htop3, htop2, htop1,
+                 str(float(int(i) * 10 / SCALE)))
+        elif int((i / 5) * 5) == i:
+            mark(math.log(i / 100.) / 2, htop3, htop2, htop1 + offset1)
+        else:
+            mark(math.log(i / 100.) / 2, htop3, htop2, htop1 + offset2)
 
-# footer
-print "  </g>"
-print "</svg>"
+    for i in range(200, 400, 2):
+        if int((i / 20) * 20) == i:
+            mark(math.log(i / 100.) / 2, htop3, htop2, htop1,
+                 str(float(int(i) * 10 / SCALE)))
+        else:
+            mark(math.log(i/100.) / 2, htop3, htop2, htop1 + offset1)
+
+    for i in range(400, 1000, 5):
+        if int((i / 10) * 10) == i:
+            if int((i / 100) * 100) == i:
+                mark(math.log(i / 100.) / 2, htop3, htop2,
+                     htop1, str(float(int(i) * 10 / SCALE)))
+            else:
+                mark(math.log(i / 100.) / 2, htop3, htop2, htop1)
+        else:
+            mark(math.log(i / 100.) / 2, htop3, htop2, htop1 + offset1)
+
+    for i in range(1000, 2000, 10):
+        if int((i / 200) * 200) == i:
+            mark(math.log(i / 100.) / 2, htop3, htop2, htop1,
+                 str(float(int(i) * 10 / SCALE)))
+        else:
+            mark(math.log(i / 100.) / 2, htop3, htop2, htop1 + offset1)
+
+    for i in range(2000, 10050, 50):
+        if int((i / 1000) * 1000) == i:
+            if int((i / 100) * 100) == i:
+                mark(math.log(i / 100.) / 2, htop3, htop2,
+                     htop1, str(float(int(i) * 10 / SCALE)))
+            else:
+                mark(math.log(i / 100.) / 2, htop3, htop2, htop1)
+        else:
+            mark(math.log(i / 100.) / 2, htop3, htop2, htop1 + offset1)
+
+    special_mark(math.log(math.pi) / 2, htop3 + offset3, htop2, htop1, 'π')
+    special_mark(math.log(math.e) / 2, htop3 + offset3, htop2, htop1, 'e')
+
+    footer()
+    return 0
+
+if __name__ == "__main__":
+    main()
