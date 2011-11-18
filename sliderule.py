d6412edf7fb6a5ce1d24d3609964a5b7416e9043
Walter Bender <walter.bender@gmail.com>
2011-11-18 12:50:33 -0500

    update license address

--- sliderule.py
+++ sliderule.py
@@ -7,10 +7,9 @@
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# You should have received a copy of the GNU General Public License
+# along with this library; if not, write to the Free Software
+# Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
 
 import pygtk
 pygtk.require('2.0')
